c8fbc45adea940664dfcde007c2245ea174bb73f
alexburlacu11 <alexandru.burlacu11@gmail.com>
2015-01-23 18:16:57 +0100

    test6

--- OcevuFTCC/tornado_main.py
+++ OcevuFTCC/tornado_main.py
@@ -239,7 +239,7 @@ if __name__ == '__main__':
     print ("Tornado server started on 8001")
     import django
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OcevuFTCC.settings")
-    print("Test5")
+    print("Test6")
     ioloop.IOLoop.instance().start()
     
     
