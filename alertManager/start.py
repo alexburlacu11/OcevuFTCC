f7ccb84be12a9cc39dac1fecdf0c9028b5e42dfe
steven7777 <etienne.pallier@irap.omp.eu>
2015-03-27 18:51:01 +0100

    OcevuFTCC ==> fgft_cc

--- alertManager/start.py
+++ alertManager/start.py
@@ -18,7 +18,7 @@ import sys
 if __name__ == '__main__':
 
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OcevuFTCC.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fgft_cc.settings")
     sys.path.append(BASE_DIR) 
 
     from alertManager.models import AlertManagerController 
--- routineManager/start.py
+++ routineManager/start.py
@@ -16,7 +16,7 @@ import sys
 if __name__ == '__main__':
 
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OcevuFTCC.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fgft_cc.settings")
     sys.path.append(BASE_DIR) 
 
     from routineManager.models import RoutineManagerController 
