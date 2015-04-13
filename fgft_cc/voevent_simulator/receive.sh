420186901fefff0e679b86ad18da3af753346da7
alexburlacu11 <alexandru.burlacu11@gmail.com>
2015-04-13 12:43:31 +0200

    changed receive

--- fgft_cc/voevent_simulator/receive.sh
+++ fgft_cc/voevent_simulator/receive.sh
@@ -2,5 +2,5 @@ IVORN=$(hostname --long | tr -d ' ')/voevent-test
 twistd comet -r --print-event \
     --verbose \
     --local-ivo=ivo://$IVORN \
-    --eventdb=voevents_received
-#--remote=68.169.57.253
+    --eventdb=voevents_received \
+	--remote=68.169.57.253
