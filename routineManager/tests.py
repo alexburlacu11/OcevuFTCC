e4037180b54dd76d1e8eb6394a34d3f96de19d10
alexburlacu11 <alexandru.burlacu11@gmail.com>
2015-04-14 19:58:20 +0200

    ex test romn

--- routineManager/tests.py
+++ routineManager/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from _code_dump.routineManager.models import Request, Album, Sequence
 
 # Create your tests here.
+
+
+# r = Request(id=1, name="x")
+# 
+# plan = Plan()
+# a = Album(id=1, plan=plan)
+# s = Sequence(id=1, album = a)
